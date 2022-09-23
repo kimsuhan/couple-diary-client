146bbe7f364221cb256d5efa2db18d45f8098078
Suhan Kim <k_yakima@naver.com>
2022-09-23 23:49:15 +0900

    dist folder

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/couple-diary-client',
   transpileDependencies: true,
   devServer: {
     proxy: {
       '/v1': {
-        target: 'http://localhost:3000',
+        target: 'http://3.35.212.237:3000',
+
         changeOrigin:true
       }
     }
